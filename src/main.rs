5e8efc7434a0ac0fe562779f6d0e158c3c0784ce
stephenwan <stephen.wan@skyscanner.net>
2020-02-12 23:33:44 +0800

    Wire up the pieces to open the last remote in browser.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate lazy_static;
 
+use crate::git_util::derive_repo_url;
+
 #[macro_use]
 mod command;
 mod git_util;
@@ -9,10 +11,18 @@ fn open_chrome(target: &str) {
     run_command!("open", "-a", "google chrome", target);
 }
 
-fn main() {
-    let s = run_command!("ls", "-alth");
+fn read_git_remote() -> Option<String> {
+    run_command!("git", "remote").get(0)
+        .and_then(|r| run_command!("git", "remote", "get-url", r).pop())
+        .map(|r| derive_repo_url(r).expect("invalid remote addr"))
+}
 
-    for (i, l) in s.iter().enumerate() {
-        println!("finished with line {}: {}", i, *l);
+fn main() {
+    match read_git_remote() {
+        Some(x) => {
+            println!("remote: {}", x);
+            open_chrome(&x)
+        }
+        None => println!("no remote is found!")
     }
 }
